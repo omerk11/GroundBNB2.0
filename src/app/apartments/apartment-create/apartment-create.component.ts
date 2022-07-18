182eeb8859e56b2297de4f11202ab5853de1bcf5
Ido Rosenberger <idoldo217@gmail.com>
2022-07-18 18:20:06 +0300

    added ap-create & downloaded google-mat

---
+++ src/app/apartments/apartment-create/apartment-create.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartment-create',
+  templateUrl: './apartment-create.component.html',
+  styleUrls: ['./apartment-create.component.css']
+})
+export class ApartmentCreateComponent
+{
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApartmentCreateComponent } from './apartments/apartment-create/apartment-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';//added manualy
+import {MatCardModule} from '@angular/material/card';//added manualy
+import {MatButtonModule} from '@angular/material/button';//added manualy
+import {MatToolbarModule} from '@angular/material/toolbar';//added manualy
+import {MatExpansionModule} from '@angular/material/expansion';//added manualy
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApartmentCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
