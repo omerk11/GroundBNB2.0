d12a251d26efb994734a1de0174abd195171b272
omerk <omerk@liveperson.com>
2022-07-23 13:43:59 +0300

    fixed updates

--- backend/controllers/apartments.controller.js
+++ backend/controllers/apartments.controller.js
@@ -3,37 +3,39 @@ const apartmentService = require('../services/apartments.service');
 
 const getAllApartments = async (req, res, next) => {
     console.log("apartment getAllApartment");
-    result = await apartmentService.getAllApartments();
+    let result = await apartmentService.getAllApartments();
     console.log(result);
     console.log("end getAllApartment");
     res.status(200).send(result);
 };
 
 const addApartment = async (req, res, next) => {
-    apartment = req.body;
+    let apartment = req.body;
     console.log("apartment addApartment");
-    result = await apartmentService.addApartment(apartment);
+    let result = await apartmentService.addApartment(apartment);
     console.log(result);
     console.log("end test");
     res.status(200).send();
 };
 
 const deleteApartmentById = async (req, res, next) => {
-    id = req.params.id;
+    let id = req.params.id;
     console.log("apartment deleteApartmentById");
     console.log(id);
-    result = await apartmentService.deleteApartmentById(id);
+    let result = await apartmentService.deleteApartmentById(id);
     console.log(result);
     console.log("end test");
     res.status(200).send('ok');
 };
 
 const updateApartmentById = async (req, res, next) => {
-    id = req.params.id;
-    // updates = JSON.parse(req.body.updates);
-    // console.log("apartment updateApartment");
-    // result = await apartmentService.updateApartmentById(id,updates);
-    // console.log(result);
+    let id = req.params.id;
+    let updates = req.body;
+    console.log("apartment updateApartment");
+    console.log(id);
+    console.log(updates);
+    let result = await apartmentService.updateApartmentById(id,updates);
+    console.log(result);
     console.log("end test");
     res.status(200).send('ok');
 };
