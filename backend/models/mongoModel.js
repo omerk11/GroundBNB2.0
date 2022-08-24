932ea41f97f75dfead1c77cc9d75af19a1715f1f
omerk11 <77975182+omerk11@users.noreply.github.com>
2022-08-24 13:46:34 +0300

    Update mongoModel.js

--- backend/models/mongoModel.js
+++ backend/models/mongoModel.js
@@ -2,7 +2,8 @@ const { ObjectID, ObjectId } = require('bson');
 
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = "mongodb+srv://admin:1q2w3e@cluster0.hd0gejt.mongodb.net/?retryWrites=true&w=majority";
+const uri = 
+      
 
 
 exports.getAll = async function (table) {
